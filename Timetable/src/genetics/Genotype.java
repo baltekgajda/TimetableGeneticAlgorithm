ad1629a309c57a063c8bcb7518b98703ed8a4e02
bartl <bartl@DESKTOP-3L80K1I.local>
2018-03-17 13:22:31 +0100

    Genotype class done

--- Timetable/src/genetics/Genotype.java
+++ Timetable/src/genetics/Genotype.java
@@ -4,34 +4,47 @@ import java.security.InvalidParameterException;
 import java.util.ArrayList;
 
 public abstract class Genotype<T> {
-	
+
 	private ArrayList<T> chromosome;
 	private int chromosomeSize;
-	
+
 	protected abstract ArrayList<T> createRandomChromosome(int chromosomeSize);
+
 	protected abstract boolean isValid(ArrayList<T> chromosome);
+
 	protected abstract ArrayList<T> repairChromosome(ArrayList<T> chromosome);
-	
+
+	protected abstract ArrayList<T> crossover(ArrayList<T> firstParent, ArrayList<T> secondParent);
+
+	protected abstract ArrayList<T> mutateChromosome(ArrayList<T> chromosome, double mutationRate);
+
 	public Genotype(int chromosomeSize) throws InvalidParameterException {
-		if(chromosomeSize>0)
+		if (chromosomeSize > 0)
 			this.setChromosomeSize(chromosomeSize);
 		else
 			throw new InvalidParameterException("ChromosomeSize less or equal to zero");
-		
-		 ArrayList<T> chrom = createRandomChromosome(chromosomeSize);
-		do {
-		 chrom = createRandomChromosome(chromosomeSize);
-		 if(!isValid(chrom))
-			 chrom=repairChromosome(chrom);
-		} while(!isValid(chrom));
+
+		ArrayList<T> chrom = createRandomChromosome(chromosomeSize);
+		if (!isValid(chrom))
+			this.chromosome = repairChromosome(chrom); // if chromosome is not valid after repair there will be a
+														// penalty points
 	}
-	
-	public Genotype(ArrayList<T> firstParent, ArrayList<T> secondParent) throws InvalidParameterException {
+
+	public Genotype(ArrayList<T> firstParent, ArrayList<T> secondParent, double mutationRate)
+			throws InvalidParameterException {
 		int size = firstParent.size();
-		if(secondParent.size()==size && size>0)
-			this.chromosomeSize=size;
+		if (secondParent.size() == size && size > 0)
+			this.chromosomeSize = size;
 		else
-			throw new InvalidParameterException("Wrong parent's ChromosomeSize");	
+			throw new InvalidParameterException("Invalid parent's ChromosomeSize");
+
+		if (mutationRate < 0 || mutationRate > 1)
+			throw new InvalidParameterException("Invalid mutationRate");
+
+		ArrayList<T> chrom = crossover(firstParent, secondParent);
+		chrom = mutateChromosome(chrom, mutationRate);
+		if (!isValid(chrom))
+			this.chromosome = repairChromosome(chrom);
 	}
 
 	public int getChromosomeSize() {
